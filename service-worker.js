2276ec43b54b73d4c6e64ee7f3c6da1d438fc161
DESKTOP-R32AB8C\Comet <sunearth.moon@outlook.com>
2021-02-22 02:55:05 +0800

    fix item

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4262d4028c7181263c2bb20184efa295.js"
+  "/precache-manifest.31b0bcf1721bb463c099e1b494d70b73.js"
 );
 
 self.addEventListener('message', (event) => {
